5824599a1f466bf08997f374de94726875051d15
MiguelMontealegre <miguelmontealegre808@gmail.com>
2025-11-27 13:34:35 -0500

    Update renewal days to 95 to force renewal testing

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DOMAIN: str = "testdomain.robin-ai.xyz"
     EMAIL: str = "admin@robin-ai.xyz"  # Default, should be overridden
     VALIDATION_INTERVAL_DAYS: int = 1
-    RENEWAL_DAYS_BEFORE_EXPIRY: int = 30
+    RENEWAL_DAYS_BEFORE_EXPIRY: int = 95
     CERT_DIR: str = "/etc/letsencrypt/live" # Default certbot path, might need to be adjustable for non-root
     WEBROOT_DIR: str = "/app/static" # Where challenge files will be stored
     
